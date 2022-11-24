d532f125a40e2a6feea6f95e2497b30fda7e40fa
half6 <christofferhalf6@gmail.com>
2022-11-24 16:25:46 +0100

    fixed about

--- imagechanger.js
+++ imagechanger.js
@@ -1,17 +1,19 @@
-const img1 = "url('/images/aboutImages/gue.jpg')";
-const img2 = "url('/images/aboutImages/testDino.png')";
-const img3 = "url('/images/aboutImages/universe.jpg')";
-const img4 = "url('/images/aboutImages/kidsvr.jpg')";
+const img1 = "url('/images/aboutImages/ai.jpg')";
+const img2 = "url('/images/aboutImages/dino1.jpg')";
+const img3 = "url('/images/aboutImages/vr.jpg')";
+const img4 = "url('/images/aboutImages/universe.jpg')";
+const img5 = "url('/images/aboutImages/teaching.jpg')";
 
-const imageArr = [img1, img2, img3, img4]
+const imageArr = [img1, img2, img3, img4, img5]
 
 const imageSection = document.querySelector('.about-explore')
 
 function changeIt(){
-    const random = Math.floor(Math.random() *imageArr.length )
+    const random = Math.floor(Math.random() *imageArr.length +1)
     imageSection.style.backgroundImage = imageArr[random]
+
 
 }
 
-setInterval(changeIt, 4000)
+setInterval(changeIt, 3100)
 
