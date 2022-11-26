b71a97ba09a656e5411c2456a43c2eb120ee10d8
half6 <christofferhalf6@gmail.com>
2022-11-26 11:48:00 +0100

    booking js

--- js/booking.js
+++ js/booking.js
@@ -1,10 +1,27 @@
 const totalPrice = document.querySelector('.total-amount')
 const select = document.querySelector('#people')
+const book = document.querySelector('#booking-submit')
 
+const bookingSuccess = document.querySelector('.booking-success')
+const closeDialog = document.querySelector('.close-dialog')
 
-
+const bookingForm = document.querySelector('.booking-form')
 
 select.addEventListener('change',(updatePrice)=>{
     totalPrice.innerText = updatePrice.target.value * 70  + ' ,-'
 })
 
+
+book.addEventListener('click', openDialog)
+closeDialog.addEventListener('click', ()=>{
+    bookingSuccess.close()
+})
+
+function openDialog(){
+    for(let i = 0; i < bookingForm.length; i++ ){
+        if(bookingForm.input.value !== "" ){
+            bookingSuccess.open()
+        }
+    }
+
+}
