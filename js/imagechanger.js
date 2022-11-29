ca62eb4463ba461ac346279222d8ff4f22bd2115
half6 <christofferhalf6@gmail.com>
2022-11-29 22:01:37 +0100

    compressing imgs

--- js/imagechanger.js
+++ js/imagechanger.js
@@ -1,10 +1,11 @@
-const img1 = "url('/images/aboutImages/ai.jpg')";
-const img2 = "url('/images/aboutImages/dino1.jpg')";
-const img3 = "url('/images/aboutImages/vr.jpg')";
-const img4 = "url('/images/aboutImages/universe.jpg')";
-const img5 = "url('/images/aboutImages/teaching.jpg')";
+const img1 = "url('/images/Museum/aboutImages/hm.jpg')";
+const img2 = "url('/images/Museum/aboutImages/dino1.jpg')";
+const img3 = "url('/images/Museum/aboutImages/vr.jpg')";
+const img4 = "url('/images/Museum/aboutImages/universe1.jpg')";
+const img5 = "url('/images/Museum/aboutImages/museum.jpg')";
+const img6 = "url('/images/Museum/aboutImages/guidedAgain.jpg')";
 
-const imageArr = [img1, img2, img3, img4, img5]
+const imageArr = [ img2, img4]
 
 const imageSection = document.querySelector('.explore')
 
