48d8a34e2039d366b64c02a3f1c94c1d24ac5135
half6 <christofferhalf6@gmail.com>
2022-11-30 12:44:52 +0100

    image at index done

--- js/imagechanger.js
+++ js/imagechanger.js
@@ -1,19 +1,18 @@
-const img1 = "url('/images/Museum/aboutImages/hm.jpg')";
-const img2 = "url('/images/Museum/aboutImages/dino1.jpg')";
-const img3 = "url('/images/Museum/aboutImages/vr.jpg')";
-const img4 = "url('/images/Museum/aboutImages/universe1.jpg')";
-const img5 = "url('/images/Museum/aboutImages/museum.jpg')";
-const img6 = "url('/images/Museum/aboutImages/guidedAgain.jpg')";
+let index = 0;
 
-const imageArr = [ img2, img4]
 
-const imageSection = document.querySelector('.explore')
 
 function changeIt(){
-    const random = Math.floor(Math.random() * imageArr.length)
-    imageSection.style.backgroundImage = imageArr[random]
-
+    setTimeout(changeIt, 4000)
+    let x;
+    const img = document.querySelectorAll('.images');
+    for( x = 0; x < img.length; x++){
+        img[x].style.display = "none"
+    }
+    index++;
+    if(index > img.length){
+        index = 1
+    }
+    img[index -1].style.display = "block"
 }
-
-setInterval(changeIt, 3000)
-
+changeIt()
